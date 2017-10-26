83940ca000ffca44b204086468929b5d4d40b826
Liliya-Tazieva <tazieva2107@gmail.com>
2017-10-26 04:32:42 +0300

    task1 ver 2.0, task2, task3

--- src/Euclid.java
+++ src/Euclid.java
@@ -1,27 +1,72 @@
 import java.util.Scanner;
 
-public class Euclid {
-    public static int gcd(int n, int m) {
+import static java.lang.Math.abs;
+import static java.lang.Math.floorMod;
 
-        if (m > n) {
-            if ((m % n) == 0)
-                return n;
+public class Euclid {
+    public static int gcd1(int a, int b) {
+        b = abs(b);
+        a = abs(a);
+        if (b > a) {
+            if ((b % a) == 0)
+                return a;
             else
-                return gcd(n, m % n);
+                return gcd1(a, b % a);
         } else {
-            if ((n % m) == 0)
-                return m;
+            if ((a % b) == 0)
+                return b;
             else
-                return gcd(m, n % m);
+                return gcd1(b, a % b);
+        }
+    }
+
+    public static int gcd2(int a, int b) {
+        b = abs(b);
+        a = abs(a);
+        if (b > a) {
+            if (floorMod(b,a) == 0)
+                return a;
+            else
+                return gcd2(a, floorMod(b,a));
+        } else {
+            if ((a % b) == 0)
+                return b;
+            else
+                return gcd2(b, floorMod(a,b));
+        }
+    }
+
+    public static int rem(int m, int n)
+    {
+        m = abs(m);
+        n = abs(n);
+        if (m > n) return m%n;
+        else return n%m;
+    }
+
+    public static int gcd3(int a, int b) {
+        int r = rem(a,b);
+        if (b > a) {
+            if (rem(b,a) == 0)
+                return a;
+            else
+                return gcd3(a, rem(b,a));
+        } else {
+            if ((a % b) == 0)
+                return b;
+            else
+                return gcd3(b, rem(a,b));
         }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int m, n;
+        int a, b;
         System.out.println("Enter numbers");
-        m = in.nextInt();
-        n = in.nextInt();
-        System.out.println("The greatest common divisor " + gcd(m, n));
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println("The greatest common divisor with % " + gcd1(a, b));
+        System.out.println("The greatest common divisor with floorMod() " + gcd2(a, b));
+        System.out.println("The greatest common divisor with rem() " + gcd3(a, b));
     }
 }
---
+++ src/Rot13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Rot13 {
+
+    public static char rot13(char c) {
+        if (c >= 'a' && c <= 'm') c += 13;
+        else if (c >= 'A' && c <= 'M') c += 13;
+        else if (c >= 'n' && c <= 'z') c -= 13;
+        else if (c >= 'N' && c <= 'Z') c -= 13;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        String s_old = args[0];
+        String s_new = "";
+        for (int i = 0; i < s_old.length(); i++) {
+            s_new = s_new + rot13(s_old.charAt(i));
+        }
+        System.out.println(s_old + " -> " + s_new);
+    }
+}
